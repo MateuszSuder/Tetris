f73768dcc8c7f5c7eb39a9c009a0312e0298a5ea
Mateusz Suder <matisu999@gmail.com>
2020-09-26 15:42:59 +0200

    Loading and options handling functions

--- src/js/index.js
+++ src/js/index.js
@@ -20,32 +20,78 @@ app.loader
     .add('b6', 'block-6.webp')
     .add('b7', 'block-7.webp');
 app.loader.onComplete.add(loadingDone);
+app.loader.onProgress.add(showProgress);
 app.loader.load();
-let blocks;
 export let blockSize;
+export let board;
+function showProgress(e) {
+    document.getElementById("loading-bar-inner").style.width = (Math.round(e.progress) / 100 * 20) + 'vw';
+}
+export function start() {
+    var _a, _b, _c;
+    if (document.getElementById("loading") != null) {
+        document.getElementById("loading").remove();
+        (_a = document.getElementById("plus")) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+            adjustLevel(true);
+        });
+        (_b = document.getElementById("minus")) === null || _b === void 0 ? void 0 : _b.addEventListener('click', () => {
+            adjustLevel(false);
+        });
+        (_c = document.getElementById("play")) === null || _c === void 0 ? void 0 : _c.addEventListener('click', () => {
+            play();
+        });
+    }
+    document.getElementById("options").style.display = "flex";
+}
+function play() {
+    let lvl = parseInt(document.getElementById('level').textContent);
+    if (lvl > 0 && lvl <= 35) {
+        if (board != undefined) {
+            board.clearBoard();
+        }
+        board = new Board(lvl);
+        document.getElementById("options").style.display = "none";
+    }
+    else {
+        document.getElementById('level').innerText = "1";
+        throw Error;
+    }
+}
+function adjustLevel(plus) {
+    let node = document.getElementById('level');
+    let lvl = 1;
+    if ((node === null || node === void 0 ? void 0 : node.textContent) != null) {
+        lvl = parseInt(node === null || node === void 0 ? void 0 : node.textContent);
+    }
+    if (plus) {
+        node.innerText = (lvl < 35 ? ++lvl : lvl).toString();
+    }
+    else {
+        node.innerText = (lvl > 1 ? --lvl : lvl).toString();
+    }
+}
 function loadingDone() {
+    var _a;
+    document.getElementById("start").style.display = "block";
+    (_a = document.querySelector("#start")) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+        start();
+    });
     blockSize = window.innerHeight / 25;
     let bg = Sprite.from(app.loader.resources['bg'].texture);
     resizeBackground(bg);
+    const blur = new filters.BlurFilter();
+    blur.blur = 5;
+    bg.filters = [blur];
     app.stage.addChild(bg);
-    new Board();
+    //new Board();
     window.onresize = () => {
         resizeBackground(bg);
     };
 }
 function resizeBackground(bg) {
-    let scale = window.innerWidth > window.innerHeight ? bg.width / bg.height : bg.height / bg.width;
-    if (window.innerWidth > window.innerHeight) {
-        bg.height = window.innerHeight;
-        bg.width = window.innerHeight * scale;
-    }
-    else {
-        bg.height = window.innerWidth * scale;
-        bg.width = window.innerWidth;
-    }
+    let scale = bg.width / bg.height;
+    bg.height = window.innerHeight;
+    bg.width = window.innerHeight * scale;
     bg.anchor.set(0.5, 1);
     bg.position.set(window.innerWidth / 2, window.innerHeight);
-    const blur = new filters.BlurFilter();
-    blur.blur = 5;
-    bg.filters = [blur];
 }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,4 @@
-import {Application, Container, Sprite, Filter, filters, Graphics} from 'pixi.js-legacy'
+import {Application, Sprite, filters} from 'pixi.js-legacy'
 import {Board} from './Board'
 
 export const app = new Application({
@@ -24,39 +24,95 @@ app.loader
 .add('b6', 'block-6.webp')
 .add('b7', 'block-7.webp')
 app.loader.onComplete.add(loadingDone);
+app.loader.onProgress.add(showProgress)
 app.loader.load();
 
-let blocks: Container[];
 export let blockSize: number;
 
+export let board: Board;
+
+function showProgress(e: any){
+    document.getElementById("loading-bar-inner")!.style.width = (Math.round(e.progress)/100 * 20) + 'vw';
+}
+
+
+export function start(){
+    if(document.getElementById("loading") != null){
+        document.getElementById("loading")!.remove()
+        document.getElementById("plus")?.addEventListener('click', () => {
+            adjustLevel(true);
+        })
+        document.getElementById("minus")?.addEventListener('click', () => {
+            adjustLevel(false);
+        })
+        document.getElementById("play")?.addEventListener('click', () => {
+            play();
+        })
+    }
+    document.getElementById("options")!.style.display = "flex";
+}
+
+function play(){
+    let lvl: number = parseInt(document.getElementById('level')!.textContent!);
+    if(lvl > 0 && lvl <= 35){
+        if(board != undefined){
+            board.clearBoard();
+        }
+        board = new Board(lvl);
+        document.getElementById("options")!.style.display = "none";
+    }else{
+        document.getElementById('level')!.innerText = "1";
+        throw Error;
+    }
+    
+}
+
+function adjustLevel(plus: boolean){
+    let node = document.getElementById('level');
+    let lvl: number = 1;
+    if(node?.textContent != null){
+        lvl = parseInt(node?.textContent);
+    }
+    if(plus){
+        node!.innerText = (lvl < 35 ? ++lvl : lvl).toString();
+    }else{
+        node!.innerText = (lvl > 1 ? --lvl : lvl).toString();
+    }
+}
+
 function loadingDone(){
+    document.getElementById("start")!.style.display = "block";
+
+    document.querySelector("#start")?.addEventListener('click', () => {
+        start();
+    })
+
     blockSize = window.innerHeight / 25;
 
     let bg = Sprite.from(app.loader.resources['bg'].texture);
     resizeBackground(bg);
+    const blur = new filters.BlurFilter()
+    blur.blur = 5;
+    bg.filters = [blur];
+
     app.stage.addChild(bg);
 
-    new Board();
+    //new Board();
 
     window.onresize = () => {
         resizeBackground(bg);
     }
 }
 
-function resizeBackground(bg: Sprite){
-    let scale: number = window.innerWidth > window.innerHeight ? bg.width / bg.height : bg.height / bg.width
 
-    if(window.innerWidth > window.innerHeight){
-        bg.height = window.innerHeight;
-        bg.width = window.innerHeight * scale;
-    }else{
-        bg.height = window.innerWidth * scale;
-        bg.width = window.innerWidth;
-    }
+
+function resizeBackground(bg: Sprite){
+    let scale: number = bg.width / bg.height;
+
+
+    bg.height = window.innerHeight;
+    bg.width = window.innerHeight * scale;
 
     bg.anchor.set(0.5, 1);
     bg.position.set(window.innerWidth / 2, window.innerHeight);
-    const blur = new filters.BlurFilter()
-    blur.blur = 5;
-    bg.filters = [blur];
 }
